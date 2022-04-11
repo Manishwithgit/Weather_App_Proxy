bb24eef1e841b6ed468f7774518f5762af13a0d5
Manishwithgit <asdarkness0@gmail.com>
2022-04-11 13:53:47 +0530

    app commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require('cors')
+const rateLimit = require('express-rate-limit')
+require('dotenv').config()
+
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 Mins
+  max: 101,
+})
+app.use(limiter)
+app.set('trust proxy', 1)
+
+// Enable cors
+app.use(cors())
+
+// Set static folder
+app.use(express.static('public'))
+
+// Routes
+app.use('/api', require('./routes'))
+
+
+//app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
